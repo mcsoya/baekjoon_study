be3440fb553143039c6d60e04497e58cc8cdb328
BictorLym <1riberty@naver.com>
2022-12-24 12:25:35 +0900

    Create riberty_25303.java
    25303

---
+++ riberty/src/week_test/riberty_25303.java
@@ -0,0 +1,23 @@
+package week_test;
+import java.util.Scanner;
+public class riberty_25303 {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+
+        int total_amount = sc1.nextInt();
+        int count = sc1.nextInt();
+        for(int i = 0; i < count; i++){
+            int price = sc1.nextInt();
+            int quantity = sc1.nextInt();
+
+            total_amount -= (price * quantity);
+        }
+        if (total_amount == 0) {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
+
